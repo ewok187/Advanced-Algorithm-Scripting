f1c98b2b9b28c5883a8790ed3196c29a65a4a904
ewok187 <k.wessalowski@gmx.de>
2017-12-26 20:46:51 +0100

    new code

--- Record Collection.js
+++ Record Collection.js
@@ -1,15 +1,15 @@
 function updateRecords(id, prop, value) {
 
-  if(!value){
-    delete collection[id][prop];
+  if(!value){  // if there is no value input
+    delete collection[id][prop]; // delete the prop of thaz collection part
     return collection;
   }
 
-  if (prop !== "tracks") {
-    collection[id][prop] = value;
+  if (prop !== "tracks") {  // if the input prop is not tracks
+    collection[id][prop] = value; // give that prop the input value
   } else {
-    if(!collection[id].hasOwnProperty("tracks")) collection[id].tracks = [];
-    collection[id].tracks.push(value);
+    if(!collection[id].hasOwnProperty("tracks")) collection[id].tracks = []; // else if that collection part has no tracks property create it as empty array
+    collection[id].tracks.push(value); // and push the input value into tracks
   }
   return collection;
 }
