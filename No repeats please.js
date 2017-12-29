5984cc549514faed6610e960f466cea45911e1b5
ewok187 <k.wessalowski@gmx.de>
2017-12-29 19:47:43 +0100

    comments

--- No repeats please.js
+++ No repeats please.js
@@ -1,20 +1,20 @@
 function permAlone(str) {
-  var arr = str.split("");
-  var result = 0;
+  var arr = str.split("");      // split str into array of the letters
+  var result = 0;               // create result variable
 
-  function swap(a,b) {
+  function swap(a,b) {          // create function swap thats swaps the letters on call
     var tmp = arr[a];
     arr[a] = arr[b];
     arr[b] = tmp;
   }
 
-  function generate(n) {
-    var regex = /([a-z])\1+/;
+  function generate(n) {        // create function generate with argument n
+    var regex = /([a-z])\1+/;   // create regexp that looks for 1 letter 2 times in a row
 
-    if(n === 1 && !regex.test(arr.join(""))) {
-      result++;
+    if(n === 1 && !regex.test(arr.join(""))) {  // if the argument is 1 and there is no letter 2times in a row
+      result++;                                 // add 1 to result
     } else {
-      for(var i = 0; i !== n; i++) {
+      for(var i = 0; i !== n; i++) {            // else swap the letters around and try again
         generate(n - 1);
         swap(n % 2 ? 0 : i, n - 1);
       }
